a50a82e3039cf5268c38dd10cb046dfd89ac265e
NurGeo <anzpro@gmail.com>
2024-03-25 16:32:47 +0500

    добавлена реализация serverResolves через функцию getCyServerResolves

--- src/infra/jwt/decoder.ts
+++ src/infra/jwt/decoder.ts
@@ -1,14 +1,13 @@
 import { BaseJwtDecoder } from 'rilata/src/infra/jwt/base-jwt-decoder';
-import { AuthJwtPayload } from '../../types';
 import { uuidUtility } from 'rilata/src/common/utils/uuid/uuid-utility';
+import { AuthJwtPayload } from '../../types';
 
 export class JwtDecoderImpl extends BaseJwtDecoder<AuthJwtPayload> {
-    constructor(public expiredTimeShiftAsMs: number) {
-      super();
-    };
+  constructor(public expiredTimeShiftAsMs: number) {
+    super();
+  }
 
-    payloadBodyIsValid(payload: AuthJwtPayload): boolean {
-      return uuidUtility.isValidValue(payload.userId) && typeof payload.telegramId === 'number'
-    }
-
+  payloadBodyIsValid(payload: AuthJwtPayload): boolean {
+    return uuidUtility.isValidValue(payload.userId) && typeof payload.telegramId === 'number';
+  }
 }
--- src/types.ts
+++ src/types.ts
@@ -5,4 +5,3 @@ export type AuthJwtPayload = {
   userId: UuidType,
   telegramId: number,
 }
-
